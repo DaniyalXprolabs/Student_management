dd9eab485ed581fcc7a166dacd14873f25c71b80
DaniyalXprolabs <daniyal.malik@xprolabs.com>
2022-05-20 06:51:59 -0700

    Learn and Implement Active Record Quries

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,43 @@
 class StudentsController < ApplicationController
   before_action :find_id, only: [:show,:destroy,:edit,:update]
   def index
-    @students=Student.all
+    #Returning all Students
+    #@students=Student.all.order(:id)
+    # Returning all students order by their first_name
+    #@students=Student.order(:first_name).all
+    # Returning all students whose id's are matching with given array
+    #@students=Student.where(id:[1,3,5,7,9])
+    # Returning all students whose id's are matching with given array
+    #@students=Student.where.not(id:[1,3,5,7,9])
+    # Returning all students whose id's are matching with given array and order them on their first_name descending order
+    #@students=Student.where.not(id:[1,3,5,7,9]).order(first_name: :desc)
+    # Returning all students whose id's are matching with given array and order them on their first_name ascending order
+    #@students=Student.where.not(id:[1,3,5,7,9]).order(first_name: :asc)
+    # Using Multiple order
+    #@students=Student.all.order(first_name: :desc).order(last_name: :asc)
+    #@students=Student.all.order(last_name: :desc)
+    #@students=Student.all.order("last_name DESC")
+    #Showing all students whose ID's are greater than 50
+    # @students=Student.where("id>50")
+    #@students=Student.where("id > ?",100)
+    #Selecting Specific fields
+    #@students=Student.select('first_name','last_name')
+    #Showing Distinct Student
+    #@students=Student.all.distinct
+    @total_students=Student.all.count
+    # Counting number of students who have distinct first name
+    @tot_students=Student.distinct.count('first_name')
+    #@female_students=Student.where(gender:'Female').count
+    #@male_students=Student.where(gender: 'Male').count
+    @gender=Student.group(:gender).count
+    # LIMIT and OFFSET queries
+    #@students=Student.limit(5).order(last_name: :asc)
+    # Showing student with second last ID
+    #@students=Student.order(id: :desc).limit(1).offset(1)
+    @students=Student.where("id>10").order(:first_name).reverse_order
+
+
+
   end
 
   def new
@@ -42,6 +78,6 @@ class StudentsController < ApplicationController
     @student=Student.find(params[:id])
   end
   def students_params
-    params.require(:student).permit(:first_name,:last_name,:email)
+    params.require(:student).permit(:first_name,:last_name,:email,:gender)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_19_143924) do
+ActiveRecord::Schema.define(version: 2022_05_20_123135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(version: 2022_05_19_143924) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "gender"
   end
 
   create_table "sub_demos", force: :cascade do |t|
