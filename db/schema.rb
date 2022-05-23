092c18695dddbb672ad06293c180596bebe14bb2
DaniyalXprolabs <daniyal.malik@xprolabs.com>
2022-05-23 01:12:29 -0700

    Single Table Inheritance is working efficently

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_23_065236) do
+ActiveRecord::Schema.define(version: 2022_05_23_080020) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -130,6 +130,14 @@ ActiveRecord::Schema.define(version: 2022_05_23_065236) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["demo_id"], name: "index_sub_demos_on_demo_id"
+  end
+
+  create_table "vehicles", force: :cascade do |t|
+    t.string "type"
+    t.string "color"
+    t.decimal "price", precision: 10, scale: 2
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "manager_histories", "managers"
